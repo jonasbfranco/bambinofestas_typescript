5851d5065de33f2c9af23e3099daf6acc9e1c46e
Jonas <jonasbfranco@hotmai.com>
2020-05-30 16:57:59 -0300

    Criando Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h2>Bambino Festas</h2>
+      <h1>Onde os Sonhos viram Festas</h1>
+      <a href="https://wa.me/5517996330704" target="_blank">
+        agende sua festa
+      </a>
+    </>
+  );
 };
 
 export default App;
